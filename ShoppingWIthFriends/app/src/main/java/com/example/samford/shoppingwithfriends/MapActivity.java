4d2b82a9af0043b15eaaceb87533241d66dbdb83
rchildre3 <rchildre3@gatech.edu>
2015-03-26 00:18:02 -0400

    Mapz

--- ShoppingWIthFriends/app/src/main/java/com/example/samford/shoppingwithfriends/MapActivity.java
+++ ShoppingWIthFriends/app/src/main/java/com/example/samford/shoppingwithfriends/MapActivity.java
@@ -7,36 +7,38 @@ import android.view.Menu;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements OnMapReadyCallback {
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
+    //private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-//        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-//                .title("Hamburg"));
-//        Marker kiel = map.addMarker(new MarkerOptions()
-//                .position(KIEL)
-//                .title("Kiel")
-//                .snippet("Kiel is cool")
-//                .icon(BitmapDescriptorFactory
-//                        .fromResource(R.drawable.ic_launcher)));
 
-        // Move the camera instantly to hamburg with a zoom of 15.
-//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-//
-//        // Zoom in, animating the camera.
-//        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
     }
 
     @Override
